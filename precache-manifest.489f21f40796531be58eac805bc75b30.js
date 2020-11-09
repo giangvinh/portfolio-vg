2aa491eddfdb7f80d6f462925384bac20c564a54
Vinh <vinh.giang@pushlishone.nl>
2020-11-09 17:30:51 +0700

    Updates

---
+++ precache-manifest.489f21f40796531be58eac805bc75b30.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "739013680e4b12c4fd073184e9fc110a",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "6a88daa6d632fa56b81e",
+    "url": "/portfolio/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "33e6a020f9efaa4e46b3",
+    "url": "/portfolio/static/js/2.c5444ead.chunk.js"
+  },
+  {
+    "revision": "63a84f5c0b19c96be47a74b765284fbf",
+    "url": "/portfolio/static/js/2.c5444ead.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a88daa6d632fa56b81e",
+    "url": "/portfolio/static/js/main.92a3c045.chunk.js"
+  },
+  {
+    "revision": "d6ca8614ab4df5b1bf68",
+    "url": "/portfolio/static/js/runtime-main.44645aae.js"
+  }
+]);
